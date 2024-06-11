1fc126b948ccde5a81c20e6d297d61b5428733be
RSkala <RichardSkala@gmail.com>
2024-06-10 19:17:33 -0700

    Create ProjectileBase C++ class; Creation of projectile Blueprints

---
+++ MegaActionPlatformer/Source/MegaActionPlatformer/Public/Projectiles/ProjectileBase.h
@@ -0,0 +1,26 @@
+// Copright Richard Skala
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+UCLASS()
+class MEGAACTIONPLATFORMER_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
